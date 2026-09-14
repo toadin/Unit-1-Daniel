7e3595c628d5417f10d45603bbc1f744c6be54cb
toadin <daniel.m.dodin@gmail.com>
2026-09-14 10:21:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+
+
+
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200) 
+
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right()
+
+
+
+
+
+turtle.done()
