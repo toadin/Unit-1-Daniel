6e63416daebb83a5d84ea119b3e60bae363a45d8
toadin <daniel.m.dodin@gmail.com>
2026-09-16 10:32:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ rectangle(100)
 
 turtle.done() """
 
-import turtle
+""" import turtle
 from turtle import *
 t = Turtle()
 
@@ -30,4 +30,34 @@ def triangle(x):
     t.forward(x)
     t.left(120)
     t.forward(x)
-triangle(200)
+triangle(200) """
+
+""" import turtle
+from turtle import *
+
+t = Turtle()
+t.shape("turtle")
+
+for i in range(60):
+    def square(x):
+        for i in range(4):
+            t.forward(x)
+            t.left(90)
+        t.left(5)
+
+    square(200) """
+
+import turtle
+from turtle import *
+
+t = Turtle()
+t.shape("turtle")
+
+def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        doubleSquares(length, 90)
+        length = length * 2
+doubleSquares(5)
+
+
